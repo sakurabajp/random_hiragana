10af0df183be15f40e415627d255ab9b32025cf3
sakurabajp <96309457+sakurabajp@users.noreply.github.com>
2022-03-30 12:07:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Number = 0
 
 while time1 != -1:
     time.sleep(1)
-    with open('ひらがな.txt', 'r', encoding="utf-8") as f:
+    with open('hiragana.txt', 'r', encoding="utf-8") as f:
         Number = Number + 1
         kw_list = f.read().split("\n")
         random1 = kw_list
